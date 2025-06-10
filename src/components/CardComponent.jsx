89380431c675ebedd0ab59f971d4c6216e3b0538
DemiusLs <dloss320@gmail.com>
2025-06-10 19:49:11 +0200

    feat:acordion

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -7,12 +7,67 @@ const CardComponent = ({ curEl }) => {
 
 
         <div className="card-body">
+
             <h5 className="card-title">{curEl.name} </h5>
-            <p className="card-text">{curEl.birth_year} {curEl.death_year} </p>
             <p className="card-text">{curEl.nationality} </p>
-            <p className="card-text">{curEl.known_for || curEl.most_famous_movies} </p>
-            <p className="card-text">{curEl.awards} </p>
-            <p className="card-text">{curEl.biography} </p>
+
+
+
+
+
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Age
+                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <p>{curEl.birth_year} {curEl.death_year} {(curEl.death_year ? curEl.death_year - curEl.birth_year : 2025 - curEl.birth_year)}anni </p>
+
+
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                            Famous for:
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            {(curEl.known_for || curEl.most_famous_movies).map((curEl, index) => <p key={index}> {curEl}</p>)}
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Awards:
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            {curEl.awards}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Biography:
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            {curEl.biography}
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>)
